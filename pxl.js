4af94976f3aca8a9b78991d9be99940a476adf95
Camden Phalen <camden2112@gmail.com>
2016-03-22 15:18:42 -0400

    add variable color

--- pxl.js
+++ pxl.js
@@ -3,7 +3,7 @@ function PxlGrid(colorArrays, rowLength) {
   
   // Converts color arrays to color objects
   this.convertColorArrays = function(colorArrays) {
-    colorObjects = [];
+    var colorObjects = [];
     for (var i = 0; i < colorArrays.length; i++) {
       colorObjects.push(new Rgb(colorArrays[i][0],
                                 colorArrays[i][1],
@@ -16,17 +16,16 @@ function PxlGrid(colorArrays, rowLength) {
   this.rowLength = rowLength;
   
   // Add a pxl to the document
-  this.makePxl = function(color) {
-    $('.pxl-container').append("<div class='pxl' style='background-color: " + color.toString() + ";'></div>");
+  this.makePxl = function(color, index) {
+    $('.pxl-container').append("<div class='pxl' data-pxlid='" + index.toString() + "' style='background-color: " + color.color(1) + ";'></div>");
   }
   
   // Converts colors to pxls in the document
   this.pxlate = function() {
     for (var i = 0; i < this.colors.length; i++) {
-      this.makePxl(this.colors[i]);
+      this.makePxl(this.colors[i], i);
     }
   }
-  
 }
 
 
@@ -36,9 +35,17 @@ function Rgb(red, green, blue) {
   this.green = green;
   this.blue = blue;
   
-  this.toString = function() {
-    return "rgb(" + this.red.toString() + ", " + this.green.toString() + ", " + 
-      this.blue.toString() + ")";
+  this.toString = function(r, g, b) {
+    return "rgb(" + r.toString() + ", " + g.toString() + ", " + 
+      b.toString() + ")";
+  }
+  
+  this.color = function(level) {
+    var factor = 1 / level;
+    var red = Math.floor(factor * this.red);
+    var green = Math.floor(factor * this.green);
+    var blue = Math.floor(factor * this.blue);
+    return this.toString(red, green, blue);
   }
 }
 
